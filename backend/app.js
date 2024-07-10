15924bca9b08f0f395ec99c3418e367f72806041
Anshu Yadav <131583641+AnshuYadav0212@users.noreply.github.com>
2024-07-10 22:30:34 +0530

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,20 @@ import morgan from "morgan";
 import nodemon from "nodemon";
 import crypto from "crypto";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+app.use(express.json());
+const __dirname = path.dirname("");
+
+const buildpath = path.join(__dirname, "../frontend/build");
+
+app.use(express.static(buildpath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(buildpath, "index.html"));
+});
+
 // Parsing json requests
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -29,13 +41,12 @@ app.use(
 
 // Use Morgan for HTTP request logging
 app.use(morgan("dev"));
+app.use(router);
 
 console.log("DBMS Backend Service");
 
 // Server Port
-const PORT = process.env.PORT || 5001;
-
-app.use(router);
+const PORT = process.env.PORT || 8080;
 
 // Connecting to mongodb Atlas then running Server
 connectDB()
@@ -45,68 +56,8 @@ connectDB()
     });
   })
   .catch((error) => {
-    console.error("Error in  connecting to MongoDB:", error);
+    console.error("Error in  connecting to MongoDB atlas:", error);
   });
-
-const merchantId = "PGTESTPAYUAT"; //process.env.PHONEPE_MERCHANT_ID;
-const secretKey = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399"; //process.env.PHONEPE_SECRET_KEY;
-
-app.post("/order", async (req, res) => {
-  try {
-    let merchantTransactionId = req.body.transactionID;
-
-    const data = {
-      merchantId: merchantId,
-      merchantTransactionId,
-      merchantUserId: req.body.MUID,
-      name: req.body.name,
-      amount: req.body.amount * 100,
-      redirectUrl: "/status?id=${merchantTransactionId}",
-      redirectMode: "POST",
-      mobileNumber: req.body.phone,
-      bankId: "SBIN",
-      paymentInstrument: {
-        type: "PAY_PAGE",
-      },
-    };
-
-    const payload = JSON.stringify(data);
-    const payloadMain = Buffer.from(payload).toString("base64");
-    const keyIndex = 1;
-    const string = payloadMain + "/pg/v1/pay" + secretKey;
-    const sha256 = crypto.createHash("sha256").update(string).digest("hex");
-    const checksum = sha256 + "###" + keyIndex;
-
-    const prod_URL =
-      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
-    const options = {
-      method: "POST",
-      url: prod_URL,
-      headers: {
-        accept: "application/json",
-        "content-Type": "application/json",
-        "X-VERIFY": checksum,
-      },
-      data: {
-        request: payloadMain,
-      },
-    };
-
-    await axios(options)
-      .then(function (response) {
-        console.log(response.data);
-        return res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  } catch (error) {
-    res.status(500).send({
-      message: error.message,
-      success: false,
-    });
-  }
-});
 
 nodemon({
   ext: "js",
